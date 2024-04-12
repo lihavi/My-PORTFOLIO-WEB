73755952deb54820b1bdd905e7b850b2a503cc9b
vivianne <viviannjoroge91@gmail.com>
2024-04-12 09:02:30 +0300

    Portfolio web

---
+++ script.js
@@ -0,0 +1,52 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onClick =() => {
+    menuIcon.classList.toggle( 'fa-xmark');
+    navbar.classList.toggle('Active');
+}
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset&&top< offset+height) {
+            navLinks.forEach.apply(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href="'+id+']').classList.add('active');
+            });
+            
+        };
+
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+     
+    window.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+ScrollReveal({
+    distance:'80px',
+    duration: 2000,
+    delay:200,
+});
+ScrollReveal().reveal('.home-content,heading', {origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact-form',{origin:'buttom'});
+ScrollReveal().reveal('.home-content h1 , .about-img' ,{origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content' ,{origin:'right'});
+
+const typed=new Typed('.multiple-text',{
+    strings: ['Software Engineer', 'Web Developer','ICT expert'],
+    typeSpeed:70,
+    backspeed:70,
+    backDelay:1000,
+    loop:true,
+});
+
